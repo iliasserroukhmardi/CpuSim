262e19e13b2d066a8a8240f0d7316cc027cc5c52
SRK <srk@localhost.localdomain>
2017-12-20 18:10:00 +0100

    almost last commit

--- projet.py
+++ projet.py
@@ -1,15 +1,14 @@
 import sys
 from numpy import binary_repr
 
-arg_A =  "initMemoire.txt"  #sys.argv[0]
-arg_B =  "programme.txt" #sys.argv[1]
+arg_A =  sys.argv[1]
+arg_B =  sys.argv[2]
 
-memoryFile = open(arg_A, "r")
+memoryFile = open(arg_A, "r+")
 programFile = open(arg_B, "r")
 
 
-
-###################################### USEFUL FUNCTIONS ######################################
+###################################### USEFUL FUNCTIONS ############################
 def bin2dec(b):
     """
         Returns decimal representation of a 2's compliment binary number
@@ -30,20 +29,28 @@ def dec2bin(d):
 
 def printRegisters():
     """
-        Prints the work registers, the instruction register and the program counter in a readable way
+        Prints the work registers, the instruction register and the program counter
+         in a readable way
     """
-    print("Registers ==>    R0: " + R[0][:4]+" "+R[0][4:] + " "*5 + "R1: " + R[1][:4]+" "+R[1][4:])
-    print("                 R2: " + R[2][:4]+" "+R[2][4:] + " "*5 + "R3: " + R[3][:4]+" "+R[3][4:] + " "*5 + "ST: "+ R[6][-1])
-    print("                 IR: " + R[4][:4]+" "+R[4][4:] + " "*5 + "PC: " + R[5][:4]+" "+R[5][4:] + "\n")
+    R0 = R[0][:4]+" "+R[0][4:]
+    R1 = R[1][:4]+" "+R[1][4:]
+    R2 = R[2][:4]+" "+R[2][4:]
+    R3 = R[3][:4]+" "+R[3][4:]
+    IR = R[4][:4]+" "+R[4][4:]
+    PC = R[5][:4]+" "+R[5][4:]
+    ST = R[6][-1]
+    print("Registers ==>    R0: " + R0 + " "*5 + "R1: " + R1)
+    print("                 R2: " + R2 + " "*5 + "R3: " + R3 + " "*5 + "ST: "+ ST)
+    print("                 IR: " + IR + " "*5 + "PC: " + PC + "\n")
 
 
 def printMemory():
     """
         Prints the Memory addresses which are not empty  in a readable way
     """
-    print("\nVALUES IN MEMORY WHICH ARE NOT EMPTY \n   ADDRESS" + 10*" "+ "VALUE" ) 
+    print("\nVALUES IN MEMORY WHICH ARE NOT EMPTY \n   ADDRESS" + 10*" "+ "VALUE") 
     for i in Memory:
-        print(i +" ("+str(bin2dec(i))+"): " + Memory[i] +" ("+str(bin2dec(Memory[i]))+")")
+        print(" "*5+str(bin2dec(i))+" "*6+":"+" "*7+str(bin2dec(Memory[i])))
     print(" \n")
 
 
@@ -55,6 +62,7 @@ def inputCheck():
     """
     try:
         v = input("PRESS 1: CONTINUE \n" + 6*" " + "2: VIEW MEMORY \n")
+        v = int(v)
         if type(v) != type(1):
             print("INVALID INPUT")
             inputCheck()
@@ -67,16 +75,34 @@ def inputCheck():
             else:
                 print("INVALID INPUT")
                 inputCheck()
-    except (NameError, SyntaxError):
+    except (NameError, SyntaxError, ValueError):
         print("INVALID INPUT")
         inputCheck()
 
+def Write2Memory():
+    """
+        This function is going to be used at the end of the simulation to write all
+        changes to memory fileThe memory file gets loaded as a dictionnary.
+        Every modification of the memory is done to this dictionnary, so at the end
+        an update of the memory file is needed, and it's done through the dictionary  
+    """
+    memoryFile.seek(0)
+    memoryFile.truncate()
+    for key in sorted(Memory.keys()):
+        memoryFile.write(str(bin2dec(key))+": "+str(bin2dec(Memory[key]))+"\n")
+    memoryFile.close()
 
 
-###################  LOAD THE PROGRAM, THE MEMORY AND THE REGISTERS  ###########################
+###################  LOAD THE PROGRAM, THE MEMORY AND THE REGISTERS  ###############
 
 #Load the program as a list of instructions
-InstrSet=[line.strip() for line in programFile]
+InstrSet = []
+for j in programFile:
+    if not j.strip():
+        continue
+    else:
+        InstrSet.append(j.strip())
+programFile.close()
 
 """
     Load the Memory as a dictionary where the Memory address is the key
@@ -84,26 +110,25 @@ InstrSet=[line.strip() for line in programFile]
 """
 Memory = {}
 for i in memoryFile:
-    i = i.rstrip().replace(" ", "").split(":")
-    Memory[dec2bin(int(i[0]))] = dec2bin(int(i[1]))
+    if not i.strip():
+        continue
+    else:
+        i = i.rstrip().replace(" ", "").split(":")
+        Memory[dec2bin(int(i[0]))] = dec2bin(int(i[1]))
 
 
 """ 
-    Create all the registers and store them iside a list
+    Create all the registers and store them inside a list
     R[0] to R[3] ===> the actual working registers  
     R[4]         ===> instruction register (IR)
     R[5]         ===> program counter      (PC)
     R[6]         ===> state register       (ST)
 """
-R = ['00000000', '00000000', '00000000', '00000000', '00000000', '00000000', '00000000']
+R = ['0'*8, '0'*8, '0'*8, '0'*8, '0'*8, '0'*8, '0'*8]
 
 
-#Set the procesor to running when script is called
-running = True
-    
 
-###################################### OPCODES (INSTRUCTIONS) ######################################
-
+###################################### OPCODES (INSTRUCTIONS) ######################
 def WRL(AB):
     """ 
         Write AB to operand of register R0 
@@ -137,9 +162,19 @@ def RD(AB):
     """
         Write the value of the Memory address given by register A to
         register B
-        (A and B are 2 bit binary numbers)  
+        (A and B are 2 bit binary numbers)
+        If the memory address doesn't exist it prints out a msg on the screen
+        and quits the simulation.
     """
-    R[int(AB[:2],2)] = Memory.get(R[int(AB[2:],2)], '00000000')
+    a = R[int(AB[2:],2)]
+    if a in Memory.keys():
+        R[int(AB[:2],2)] = Memory.get(a)
+    else:
+        running = False
+        b = str(bin2dec(a))
+        print("The memory address "+b+" hasn't been initialized")
+        print("Make sure that the address "+b+" is in the memory file")
+        quit() 
 
 def ADD(AB):
     """
@@ -195,6 +230,9 @@ def END(AB = '0000'):
     """
     global running 
     running = False
+    quit("\n"+15*'-'+"Programm succesfully terminated"+15*'-')
+
+
 
 """
     Store functions in a dictionary, where the keys are the actual opcodes and the 
@@ -204,38 +242,57 @@ opcodes = {'0000':WRL, '0001':WRH, '0010':MOV, '0011':STR, '0100':RD, '0101':ADD
      '0110':INV, '0111':JP, '1000':JZ, '1001':JNZ, '1010':END}
 
 
-###################################### Execute  ######################################
+###################################### Execute  ####################################
+
+def fetch():
+    """
+        Fetch the instruction pointed by the programm counter, and add one to the 
+        program counter
+    """
+    R[4] = InstrSet[bin2dec(R[5])]
+    R[5] = dec2bin(bin2dec(R[5]) + 1)
+
+def decode():
+    """
+        Determine the type of the instruction which is going to be executed.
+        This part of the cycle gets simulated only by returning the name of our 
+        defined instruction
+    """
+    return opcodes[R[4][:4]].__name__ + " " +R[4][4:] 
+
+def execute():
+    """
+        Execute the decoded instruction.
+    """
+    opcodes[R[4][:4]](R[4][4:])
+
 
 running = True
 def run():
+    """
+        run the program using the fetch-decode-execute loop cycle
+    """
     i = 1
     while running:
         #Fetch
         print("Cycle: " + str(i) + " ==> " + "FETCH")
         printRegisters()
-        #inputCheck()
-        R[4] = InstrSet[bin2dec(R[5])]
+        fetch()
+
         #Decode
         print("Cycle: " + str(i) + " ==> " + "DECODE")
         printRegisters()
-        #inputCheck()
+        instuction = decode()
+
         #Execute
         print("Cycle: " + str(i) + " ==> " + "EXECUTE")
         printRegisters()
-        print("Decoded Instruction: " + opcodes[R[4][:4]].__name__ + " " +R[4][4:])
+        print("Decoded Instruction: " + instuction )
         #inputCheck()
-        opcodes[R[4][:4]](R[4][4:])
+        execute()
+        
         i += 1
-        R[5] = dec2bin(bin2dec(R[5]) + 1)
-
-
+       
 run()
+Write2Memory()
 
-
-"""
-def Fetch():
-    R[4] = InstrSet[bin2dec(R[5])]
-    R[5] = dec2bin(bin2dec(R[5]) + 1)
-
-def Decode():
-"""
